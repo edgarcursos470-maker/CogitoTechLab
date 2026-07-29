83e2d4ac0d8050bc925da6177b85d8fc0b990d11
EdgarPF <edgfranca@hotmail.com>
2026-07-29 09:08:38 -0300

    ex.9_1

---
+++ cursos_em_video/curso_em_video_js/aula11/ex009.js
@@ -0,0 +1,11 @@
+//condição composta
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
+
