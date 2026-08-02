4b505e9a636e7441fb1ee30d7331b0cfa2ef5946
EdgarPF <edgfranca@hotmail.com>
2026-08-01 22:09:33 -0300

    relogioExu

--- cursos_em_video/curso_em_video_js/exercíciosJS/seuExu/script.js
+++ cursos_em_video/curso_em_video_js/exercíciosJS/seuExu/script.js
@@ -1,1 +1,21 @@
-alert("Axé!")
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var exuImage = window.document.getElementById('exuImage')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos`
+        document.body.style.fontFamily = 'Arial, Helvetica, sans-serif'
+
+    if (hora >= 0 && hora < 12){
+        exuImage.src = 'imagens/trancaRuas.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18){
+        exuImage.src = 'imagens/mariaPadrilha.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        exuImage.src = 'imagens/joaoCaveira.png'
+        document.body.style.background = '#050b14'
+    };
+}
